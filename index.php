e2b7b5fba621163d56ced9aed7bbaa3a1842304f
janith nirmal <113751270+janithnirmal@users.noreply.github.com>
2023-05-07 01:07:20 +0530

    design updated
    Co-Authored-By: kavindu202 <117804108+kavindu202@users.noreply.github.com>

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Todo List</title>
+    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="css/main.css" />
+    <link rel="stylesheet" href="css/bootstrap.css" />
+</head>
+
+<body style="background:linear-gradient(135deg,#153677,#4e085f);">
+    <div class="container">
+        <div class="row m-0">
+            <div class="col-12 p-0">
+                <div class="row m-0">
+                    <div class="col-12 text-center mt-5">
+                        <h1 class="text-white">Todo List</h1>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="col-12 p-0">
+                <div class="row m-0">
+                    <div class="col-3 p-0 my-4 d-flex justify-content-center align-items-center">
+                        <div class="card" style="width: 18rem;">
+                            <div class="card-body">
+                                <div class="row m-0">
+                                    <div class="col-12 p-0 pt-1">
+                                        <h5 class="card-subtitle mb-2 text-body-secondary">Task Title</h5>
+                                    </div>
+                                </div>
+
+                                <span class="text-primary">End Date &nbsp;&nbsp;&nbsp; 2022/05/06</span>
+
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+    </div>
+</body>
+
+</html>
