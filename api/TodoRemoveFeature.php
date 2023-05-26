dc7e44f3f85146bc69561a98b1f697e436546ac4
kavindu202 <kavindu.sasanka962@gmail.com>
2023-05-26 23:32:36 +0530

    remove todo update

--- api/TodoRemoveFeature.php
+++ api/TodoRemoveFeature.php
@@ -1,7 +1,7 @@
 <?php
 
 
-$request = $_GET["removeTodoId"]; 
+$request = $_POST["removeTodoId"];
 $requestObject = json_decode($request);
 
 
@@ -10,26 +10,24 @@ require("../app/errorResponseSender.php");
 require("../app/inputValidator.php");
 
 
-$responseObject = new stdClass(); 
+$responseObject = new stdClass();
 
-$responseObject->error = "faild";
+$responseObject->status = "faild";
 
 $validator = new Validator($requestObject);
 $errors = $validator->validator();
 foreach ($errors as $key => $value) {
-   $responseObject->error = $value;
-   ErrorSender::sendError($responseObject);
+   if ($value) {
+      $responseObject->error = $value;
+      ErrorSender::sendError($responseObject);
+   }
 }
 
 $todo_id = $requestObject->todo_id;
 
 $database = new DB();
 $query = "DELETE FROM `todo` WHERE `id` = ?";
-$setup = $database->prepare($query,"i",array($todo_id));
+$setup = $database->prepare($query, "i", array($todo_id));
 
-$responseObject->states = "success";
+$responseObject->status = "success";
 ErrorSender::sendError($responseObject);
-
-
-
-
