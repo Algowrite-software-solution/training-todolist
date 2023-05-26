bf6917fa869730f3267a005d407e10b4417de1d7
Nisal_Uthsara <nisaluthsara1@gmail.com>
2023-05-27 00:04:44 +0530

    todoRemoveFeature-Frontend

---
+++ ToDoRemovingFeature.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Todo List</title>
+    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="css/main.css" />
+    <link rel="stylesheet" href="css/bootstrap.css" />
+</head>
+
+<body>
+    <div>
+        <button onclick="todoRemove();" id="todo_remove" type="button" class="btn btn-secondary">Remove TODO</button>
+    </div>
+</body>
+
+</html>
