3f49b033114798335006ae50921e650079e89dc1
janith nirmal <113751270+janithnirmal@users.noreply.github.com>
2023-05-26 12:19:01 +0530

    ui fix
    Co-Authored-By: NisalUthsara <117739668+NisalUthsara@users.noreply.github.com>

--- api/ToDoAddingFeature.php
+++ api/ToDoAddingFeature.php
@@ -11,34 +11,7 @@
 </head>
 
 <body>
-    <!-- Main Div -->
-    <div class="d-flex justify-content-center align-items-center bg-info" style="height: 700px;">
-        <!-- Main Box -->
-        <div class="w-25 h-50 bg-white d-flex flex-column justify-content-center align-items-center rounded-4  ">
-            <!-- Title : ADD YOUR TODO -->
-            <div class="w-100 d-flex justify-content-center align-items-center ">
-                <p class="title fs-2 fw-medium ">Add Your todo</p>
-            </div>
-            <!-- Form Section-->
-            <div class="w-100 d-flex justify-content-center align-items-center ">
-                <form class="w-75 ">
-                    <label class="form-label d-flex " for="title">
-                        <input type="text " class="form-control " id="title">
-                        <span class="w-25 text-center ">Title</span>
-                    </label>
-                    <label class="form-label d-flex " for="date">
-                        <input type="date" class="form-control " id="due_date">
-                        <span class="w-25 text-center ">Due date</span>
-                    </label>
-                    <label class="form-label d-flex">
-                        <input type="time" class="form-control " id="due_time">
-                        <span class="w-25 text-center ">Due time</span>
-                    </label>
-                    <button type="submit" onclick="todoAdd();" class="btn bg-info text-white w-100 ">Add Task</button>
-                </form>
-            </div>
-        </div>
-    </div>
+    
 
 
     <script src="../js/script.js"></script>
--- index.php
+++ index.php
@@ -17,6 +17,37 @@
 <body>
     <div class="container" style="background:linear-gradient(135deg,#153677,#4e085f);">
         <div class="row m-0">
+            <!-- Main Div -->
+            <div class="p-0 col-lg-6 offset-lg-3 col-12">
+                <div class="row p-3 m-0">
+
+                    <!-- Main Box -->
+                    <div class="h-50 bg-white d-flex flex-column justify-content-center align-items-center rounded-4">
+                        <!-- Title : ADD YOUR TODO -->
+                        <div class="w-100 d-flex justify-content-center align-items-center ">
+                            <p class="title fs-2 fw-bold ">Add Your todo</p>
+                        </div>
+                        <!-- Form Section-->
+                        <div class="w-100 d-flex justify-content-center align-items-center my-3">
+                            <form class="w-100">
+                                <label class="form-label d-flex " for="title">
+                                    <span class="w-25 fw-bold ">Title</span>
+                                    <input type="text " class="form-control " id="title">
+                                </label>
+                                <label class="form-label d-flex " for="date">
+                                    <span class="w-25 fw-bold ">Due date</span>
+                                    <input type="date" class="form-control " id="due_date">
+                                </label>
+                                <label class="form-label d-flex">
+                                    <span class="w-25 fw-bold ">Due time</span>
+                                    <input type="time" class="form-control " id="due_time">
+                                </label>
+                                <button type="submit" onclick="todoAdd();" class="btn bg-info text-white w-100 ">Add Task</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <div class="col-12 p-0">
                 <div class="row m-0">
                     <div class="col-12 text-center mt-5">
